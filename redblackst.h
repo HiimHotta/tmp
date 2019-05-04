62992b55c9fdf21130468cbcc747be05b2c187f8
HiimHotta <daniel.y.hotta@gmail.com>
2019-05-04 15:45:18 -0300

    Update redblackst.h
    IDENTICO COELHO

--- redblackst.h
+++ redblackst.h
@@ -14,34 +14,34 @@
 /* 
  * REDBLACKST
  *
- * Representa uma tabela de símbolos (ST) ordenada "genérica".
+ * Representa uma tabela de sÃ­mbolos (ST) ordenada "genÃ©rica".
  *
- * Permite as operações usuais: put(), get(), contains(), delete(), size() 
+ * Permite as operaÃ§Ãµes usuais: put(), get(), contains(), delete(), size() 
  * e isEmpty().
  *
- * Fornece algumas operações para tabelas de símbolos ordenadas: min(), 
+ * Fornece algumas operaÃ§Ãµes para tabelas de sÃ­mbolos ordenadas: min(), 
  * max(), rank(), select(), deleteMin() e deleteMax().
  *
- * Também fornece a operação keys() para iterar todas as chaves.
+ * TambÃ©m fornece a operaÃ§Ã£o keys() para iterar todas as chaves.
  *
- * Uma tabela de símbolos implementa a abstração "array associativo":
- * ao associar um valor a uma chave que já está na tabela de símbolos,
- * a convenção é substituir o valor antigo pelo novo valor.
+ * Uma tabela de sÃ­mbolos implementa a abstraÃ§Ã£o "array associativo":
+ * ao associar um valor a uma chave que jÃ¡ estÃ¡ na tabela de sÃ­mbolos,
+ * a convenÃ§Ã£o Ã© substituir o valor antigo pelo novo valor.
  *
- * Ao contrário de java.util.Map, essa implementação usa a convenção de que
- * os valores não podem ser NULL - associar o valor de uma chave para NULL
- * é equivalente a excluir a chave da tabela de símbolos.
+ * Ao contrÃ¡rio de java.util.Map, essa implementaÃ§Ã£o usa a convenÃ§Ã£o de que
+ * os valores nÃ£o podem ser NULL - associar o valor de uma chave para NULL
+ * Ã© equivalente a excluir a chave da tabela de sÃ­mbolos.
  * 
- * Esta implementação usa uma árvore rubro-negra.
- * Requer que uma função compar() seja argumento do construtor.
+ * Esta implementaÃ§Ã£o usa uma Ã¡rvore rubro-negra.
+ * Requer que uma funÃ§Ã£o compar() seja argumento do construtor.
  *
- * As operações put(), get(), delete(), deleteMin(), deleteMax(), 
+ * As operaÃ§Ãµes put(), get(), delete(), deleteMin(), deleteMax(), 
  * contains(), select(), rank(), min(), max() consomem  tempo 
- * logarítmico.
- * As operações size(), isEmpty() consomem tempo constante.
+ * logarÃ­tmico.
+ * As operaÃ§Ãµes size(), isEmpty() consomem tempo constante.
  *
- * Para documentação adicional, veja 
- * https://algs4.cs.princeton.edu/33balanced, Seção 3.3
+ * Para documentaÃ§Ã£o adicional, veja 
+ * https://algs4.cs.princeton.edu/33balanced, SeÃ§Ã£o 3.3
  * Algorithms, 4th Edition, por Robert Sedgewick e Kevin Wayne.
  */
 
@@ -64,76 +64,92 @@ typedef struct redBlackST *RedBlackST;
  *  INIT(COMPAR)
  *
  *  cria uma ST vazia 
- *  compar() é ponteiro para uma função que retorna um inteiro tal que
+ *  compar() Ã© ponteiro para uma funÃ§Ã£o que retorna um inteiro tal que
  *
  *      compar(key1, key2) retorna um inteiro < 0 se key1 <  key2
  *      compar(key1, key2) retorna um 0           se key1 == key2
  *      compar(key1, key2) retorna um inteiro > 0 se key1 >  key2
  */
-RedBlackST initST(int (*compar)(const void *this, const void *that));
+RedBlackST
+initST(int (*compar)(const void *this, const void *that));
 
-/* libera todo o espaço usado pela ST */
-void freeST(RedBlackST st);
+/* libera todo o espaÃ§o usado pela ST */
+void  
+freeST(RedBlackST st);
 
 /*------------------------------------------------------------*/
 /*
- * Permite as operações usuais: put(), get(), contains(), delete(),
+ * Permite as operaÃ§Ãµes usuais: put(), get(), contains(), delete(),
  * size() e isEmpty().
  */
 
 /* coloca um par KEY-VAL na ST */
-void put(RedBlackST st, const void *key, size_t nKey, const void *val, size_t nVal);
+void  
+put(RedBlackST st, const void *key, size_t nKey, const void *val, size_t nVal);
 
 /* retorna o valor associado a KEY */
-void *get(RedBlackST st, const void *key);
+void * 
+get(RedBlackST st, const void *key);
 
-/* retorna TRUE se KEY esta na ST e FALSE em caso contrário */
-Bool contains(RedBlackST st, const void *key);
+/* retorna TRUE se KEY esta na ST e FALSE em caso contrÃ¡rio */
+Bool
+contains(RedBlackST st, const void *key);
 
 /* remove da ST a chave KEY */
-void delete(RedBlackST st, const void *key);
+void
+delete(RedBlackST st, const void *key);
 
 /* retorna o numero de itens na ST */
-int size(RedBlackST st);
+int
+size(RedBlackST st);
 
-/* retorna TRUE se ST esta vazia e FALSE em caso contrário */
-Bool isEmpty(RedBlackST st);
+/* retorna TRUE se ST esta vazia e FALSE em caso contrÃ¡rio */
+Bool
+isEmpty(RedBlackST st);
 
 /*------------------------------------------------------------*/
 /*
- * Fornece algumas operações para tabelas de símbolos ordenadas: 
+ * Fornece algumas operaÃ§Ãµes para tabelas de sÃ­mbolos ordenadas: 
  * min(), max(), rank(), select(), deleteMin() e deleteMax().
  */
 
 /* menor chave na ST */
-void *min(RedBlackST st);
+void *
+min(RedBlackST st);
 
 /* maior chave na ST */
-void *max(RedBlackST st);
+void *
+max(RedBlackST st);
 
 /* numero de chaves menores que key */
-int rank(RedBlackST st, const void *key);
+int
+rank(RedBlackST st, const void *key);
 
 /* remove menor chave */
-void deleteMin(RedBlackST st);
+void
+deleteMin(RedBlackST st);
 
 /* remove menor chave */
-void deleteMax(RedBlackST st);
+void
+deleteMax(RedBlackST st);
 
-/* a k-ésima chave da ST */
-void *select(RedBlackST st, int k);
+/* a k-Ã©sima chave da ST */
+void *
+select(RedBlackST st, int k);
 
 /*------------------------------------------------------------*/
 /*
- * Também fornece a operação keys() para iterar todas as chaves.
+ * TambÃ©m fornece a operaÃ§Ã£o keys() para iterar todas as chaves.
  */
 
 /* all of the keys, as an Iterable */
-void *keys(RedBlackST st, Bool init);
+void *
+keys(RedBlackST st, Bool init);
 
 /***************************************************************************
  *  Check integrity of red-black tree data structure.
  ***************************************************************************/
-Bool check(RedBlackST st);
+Bool 
+check(RedBlackST st);
 
 #endif /* _REDBLACKST_H */
